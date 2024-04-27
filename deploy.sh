4c9dcb6cc71561e9787a5582ecf5442963e9f9d1
atlfsj <3321713512@qq.com>
2024-04-27 17:28:28 +0800

    解决Mixed Content问题

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf dist &&
 yarn build &&
 git add . &&
-git commit -m "后端部署到服务器" &&
+git commit -m "解决Mixed Content问题" &&
 git push -u origin new 
 # echo https://atlfsj.github.io/tomato-monitor/dist/index.html
