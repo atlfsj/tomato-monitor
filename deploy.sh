a3176848d32082dd41376991e21d6af3c670fc9f
atlfsj <3321713512@qq.com>
2024-04-30 21:48:56 +0800

    解决Network Error, 'AxiosError'问题

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf dist &&
 yarn build &&
 git add . &&
-git commit -m "解决cors问题" &&
+git commit -m "解决Network Error, 'AxiosError'问题" &&
 git push -u origin new 
 # echo https://atlfsj.github.io/tomato-monitor/dist/index.html
